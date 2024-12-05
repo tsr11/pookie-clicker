447a4ffe23ea3650e980f0cee5d010dac5b8e832
tsr11 <166618480+tsr11@users.noreply.github.com>
2024-12-05 21:13:21 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var cookies = 0;
+var increment = 1;
+var cps = 0;
+quotes = [
+  "I love balls",
+  "Them balls are cool",
+  "Yaargh, skibidi",
+  "Rizzler's gyat",
+];
+
+function skibidiya() {
+  cookies += increment;
+  document.getElementById("pookies").innerHTML = cookies;
+  document.getElementById("quotesbox").innerHTML =
+    quotes[Math.floor(Math.random() * 4)];
+}
+
+function buycursor() {
+  if (cookies >= 15) {
+    cookies -= 15;
+    cps += 0.1;
+    document.getElementById("pps").innerHTML = cps;
+    document.getElementById("pookies").innerHTML = cookies;
+  }
+}
